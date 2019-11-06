fe3bad533b0d38fe007d5748ec35fb2ed15c3fce
Ethan Freestone <ethan.freestone@k-int.co.uk>
2019-11-06 11:52:42 +0000

    Slight neaten of custprops entry

--- src/components/DirectoryEntryForm/components/CustPropsListField.js
+++ src/components/DirectoryEntryForm/components/CustPropsListField.js
@@ -129,7 +129,6 @@ export default class CustPropsListField extends React.Component {
       <FieldComponent
         data-test-custprop-value
         id={`edit-custprop-${i}-value`}
-        label={<FormattedMessage id="ui-directory.information.local.custPropValue" />}
         onChange={handleChange}
         value={controlledFieldValue}
         error={!isEmpty(errors) ? errors[custprop.value] : undefined}
@@ -143,16 +142,16 @@ export default class CustPropsListField extends React.Component {
     const custPropsList = this.state.custprops.map((custprop, i) => {
       const header = custprop.label;
       return (
-        <EditCard
-          header={header}
-          key={custprop.value}
-        >
-          <Row>
-            <Col xs={12} md={6}>
+        <Row>
+          <Col xs={12} md={6}>
+            <EditCard
+              header={header}
+              key={custprop.value}
+            >
               {this.renderCustPropValue(custprop, i)}
-            </Col>
-          </Row>
-        </EditCard>
+            </EditCard>
+          </Col>
+        </Row>
       );
     });
     return custPropsList;
